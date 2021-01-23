802d36b0929beda2e8caad54ab17696085271b8a
Matt <earthc0137@gmail.com>
2021-01-23 15:29:27 -0500

    NBA Update 2021-01-23 15:29:25

--- Scripts/Sports/NBA/YouTube- SPORTS ANALYTICS PROJECT - Importing 2021 NBA Data - R programming.R
+++ Scripts/Sports/NBA/YouTube- SPORTS ANALYTICS PROJECT - Importing 2021 NBA Data - R programming.R
@@ -1,2 +1,7 @@
 library(readr)
+library(dplyr)
+
+games_2021 <- read_csv("https://raw.githubusercontent.com/MattC137/Open_Data/master/Data/Sports/NBA/NBA_2021_Games.csv")
+games_2021 <- as.data.frame(games_2021)
+View(games_2021 %>% filter(Result == "TBD"))
 
